b8ad1bf8670053c0c5ccecbdf5460d9814dcec82
blade <blade@nodies.org>
2024-04-10 19:32:16 -0500

    update and go

--- src/generate-keys.ts
+++ src/generate-keys.ts
@@ -1,11 +1,6 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
-
-import {JsonRpcProvider} from "@pokt-foundation/pocketjs-provider";
-
 import * as Path from "path";
-import {setPoktProvider} from "./di";
-import {appStakeRetry, appUnstakeRetry} from "./pokt/appStakeTxs";
 import {askQuestion} from "./common/ask_question";
 import {KeyManager} from "@pokt-foundation/pocketjs-signer";
 
@@ -18,16 +13,15 @@ const rl = readline.createInterface({
 
 async function main() {
     const appStakeAmount = await askQuestion(rl, 'How many app stakes to generate?: ');
-
     let csvOutput = 'privateKey\n'
-
     const privateKeys: string[] = []
     for(let i = 0; i < parseInt(appStakeAmount); i++) {
         const km = await KeyManager.createRandom()
         privateKeys.push(km.getPrivateKey())
     }
     csvOutput += privateKeys.join("\n")
-    fs.writeFileSync(csvOutput, Path.join(__dirname, `../input/new-app-private-keys-${new Date().toISOString()}.csv`.replace(/:/g,"_"), csvOutput, 'utf8'))
+    const outputPath = Path.join(__dirname, `../input/new-app-private-keys-${new Date().toISOString()}.csv`.replace(/:/g,"_"))
+    fs.writeFileSync(outputPath, csvOutput, 'utf8')
     rl.close()
 }
 
